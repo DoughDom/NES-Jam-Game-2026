415f857fb37eb769258fe06fdeb78f5a6f7ccf05
DoughDom <dominic.survine@gmail.com>
2026-05-31 14:21:33 -0700

    removed the eventhandler references gamescene script

--- nes-jam-project/scenes/scenes_scripts/game_scene.gd
+++ nes-jam-project/scenes/scenes_scripts/game_scene.gd
@@ -1,11 +1,8 @@
 extends Node2D
 class_name GameScene
 
-var eventHandler:EventHandler
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	eventHandler = get_node("EventHandler")
 	
 	pass # Replace with function body.
 
