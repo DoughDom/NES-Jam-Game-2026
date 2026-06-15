fc1cc0284ee4be9154410f7610cc47f70eb443a1
DoughDom <dominic.survine@gmail.com>
2026-06-15 01:06:06 -0700

    battle stuff, just updating for version control

--- nes-jam-project/scenes/scenes_scripts/game_scene.gd
+++ nes-jam-project/scenes/scenes_scripts/game_scene.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name GameScene
 
-
+@export var encounter: Encounter
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#battleScene.initialize([testEntity])
@@ -9,7 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
-	pass
+	if Input.is_action_just_pressed("select"):
+		Events.combatTriggered.emit(encounter)
 	
 func _enter_scene() -> void:
 	pass
