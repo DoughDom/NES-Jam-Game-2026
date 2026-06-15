72e3e62cba5cdf66f6012bb407572989ec566027
Hayderr811 <haydenweber811@gmail.com>
2026-06-15 00:51:08 -0700

    my changes

--- nes-jam-project/scenes/combatHandler.gd
+++ nes-jam-project/scenes/combatHandler.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 var _activeBattle: BattleScene = null
 
-func _ready() -> void:
-	Events.combatTriggered.connect(startCombat)
+#func _ready() -> void:
+#	Events.combatTriggered.connect(startCombat)
 	
 	
-func startcombat()
+#func startcombat()
